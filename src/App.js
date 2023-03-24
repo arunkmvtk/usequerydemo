24eca3739918d37f281d9749db0bbe1636ee2c85
arunkmvtk <arunkm.vadakara541@gmail.com>
2023-03-24 18:16:21 +0530

    usequery file added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import RQFetching from './components/RQFetching';
 
 function App() {
   return (
     <div className="App">
      USEQUERY DEMO
+     <RQFetching/>
     </div>
   );
 }
---
+++ src/components/RQFetching.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React from 'react';
+import { useQuery } from 'react-query';
+
+ const RQFetching = () => {
+
+    const {isLoading,error,data}= useQuery('users',() => 
+        axios.get('https://jsonplaceholder.typicode.com/users')
+    )
+    // console.log("data",data?.data);
+  return (<>
+      <h1>USER LIST</h1>
+      <div>
+{isLoading && <h1>Loading...</h1>}
+{error && <h1>{error.message}</h1>} 
+{
+    data?.data.map(user => (
+        <div key={user.id}>
+            {user.name}
+        </div>
+    ))
+}
+    </div>
+    </>
+  )
+  
+}
+export default RQFetching;
